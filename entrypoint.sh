7b46d763d934b219a48fcf2062b45e2785ed9441
Bobby DeVeaux <bobby.deveaux@riversafe.co.uk>
2024-05-22 16:46:25 +0100

    removing debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ exitCode=${PIPESTATUS[0]}
 
 scanId=(`grep -E '"(ID)":"((\\"|[^"])*)"' $output_file | cut -d',' -f1 | cut -d':' -f2 | tr -d '"'`)
 
-echo "bobby testing" >> $GITHUB_OUTPUT
 
 echo "cxcli=$(cat $output_file | tr -d '\r\n')" >> $GITHUB_OUTPUT
 
